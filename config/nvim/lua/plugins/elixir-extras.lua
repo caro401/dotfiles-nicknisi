f82ff2f132ce08bb523e22bdac2948bc5f49e609
Caro Appleby <caroline.appleby@cantab.net>
2025-01-16 17:14:54 +0000

    start nvim config from LazyVim

---
+++ config/nvim/lua/plugins/elixir-extras.lua
@@ -0,0 +1,19 @@
+--- https://dev.to/abreujp/enhancing-elixir-development-in-lazyvim-quick-documentation-access-c3e
+
+return {
+  {
+    "emmanueltouzery/elixir-extras.nvim",
+    lazy = true,
+    ft = "elixir",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>cd", function() require("elixir-extras").elixir_view_docs({}) end, desc = "Elixir View Docs" },
+      { "<leader>cc", function() require("elixir-extras").module_complete() end, desc = "Elixir Module Complete" },
+    },
+    config = function()
+      require("elixir-extras").setup_multiple_clause_gutter()
+    end,
+  },
+}
